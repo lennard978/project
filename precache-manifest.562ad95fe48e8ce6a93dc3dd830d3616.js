9b1c9624fb84174e2b9458bf837335376240849d
Lennard978 <shparavalo78nenad@yahoo.com>
2020-09-29 12:23:05 +0200

    Updates

---
+++ precache-manifest.562ad95fe48e8ce6a93dc3dd830d3616.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20320b62d50f5a0440dc07068ae0deb4",
+    "url": "/project/index.html"
+  },
+  {
+    "revision": "782deade2802e94c6183",
+    "url": "/project/static/css/main.387a47aa.chunk.css"
+  },
+  {
+    "revision": "3cea50966585f15dc6c7",
+    "url": "/project/static/js/2.c7482b7f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/project/static/js/2.c7482b7f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "782deade2802e94c6183",
+    "url": "/project/static/js/main.6e27f631.chunk.js"
+  },
+  {
+    "revision": "6d3b6d49744cfd603aa5",
+    "url": "/project/static/js/runtime-main.adcb654e.js"
+  },
+  {
+    "revision": "409c7f79a42e56c785f50ed37535f0be",
+    "url": "/project/static/media/Montserrat-Light.409c7f79.ttf"
+  },
+  {
+    "revision": "9daad6c578f5c99a40bed3821df03eda",
+    "url": "/project/static/media/travel.9daad6c5.jpg"
+  }
+]);
