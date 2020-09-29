a81076b93c25c4e46fa2dfbbbffbf59a1e7190ce
Nenad Sparavalo <nenadsparavalo@Nenads-MBP.fritz.box>
2020-09-29 15:13:26 +0200

    ?

--- src/components/Navigation/BurgerBtn.js
+++ src/components/Navigation/BurgerBtn.js
@@ -2,7 +2,12 @@ import React from "react";
 import styled from "./burgerBtn.module.css";
 export default function BurgerBtn() {
 	return (
-		<div className={styled.btn}>
+		<div
+			className={styled.btn}
+			onClick={() => {
+				alert("hello");
+			}}
+		>
 			<div className={styled.line}></div>
 			<div className={styled.line}></div>
 		</div>
